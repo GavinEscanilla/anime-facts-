e0b51fa2ad7cd42985e1b7934b5d2e5f75529b83
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-05-22 11:13:08 -0400

    test

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const searchinput = document.getElementById('search-input');
 const searchbtn = document.getElementById('searchbtn');
 const animefacts = "https://anime-facts-rest-api.herokuapp.com/api/v1";
-
-const  fetchData = async (input) => {
+const  fetchData = async () => {
     const err = 'Pokémon not found';
     try {
       const res = await fetch(animefacts);
       const data = await res.json();
-      console.log('data')
+      console.log('data');
     
-    } catch (err) {
-       err = "Pokémon not found";
-      alert(err);
+    } catch () {
+       
     }
     
   };
+  fetchData();
+  /*
   searchbtn.addEventListener('click', () => {
     fetchData(searchinput.value.toLowerCase().trim())
-  });
+  });*/
+  
