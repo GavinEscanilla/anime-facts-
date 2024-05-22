acdea031813fa7f2928002ce9023183d81c09c0c
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-05-22 14:33:40 -0400

    still stuck on still trying to get api to run

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-//import axios from "axios";
-const input = document.getElementById("search-input");
+const axios = require('axios');
+//const input = document.getElementById("search-input");
 const search = document.getElementById("search-button");
 const img = document.getElementById('img');
-const animefacts = "Mzk3MTU0OTMyOTU4MTY3MDUx.MTcxNjM5ODA4OQ--.c395354176";
+const url = "https://waifu.it/api/v4/angry"; 
+/*
+Replace "Your-API-Token" with the token you got from the Kohai Bot and the endpoint.
+*/
 
-const url = "https://waifu.it/api/v4/angry";
-const fetchData = async () => {
-  try {
-    const { data } = await axios.get(url, { headers: {
-        Authorization: "Mzk3MTU0OTMyOTU4MTY3MDUx.MTcxNjM5ODA4OQ--.c395354176",
-    } });
-    return data;
-} catch (err) {
-    throw new Error(err.message);
-}
+const data = async () => {
+    try {
+        const { data } = await axios.get(url, { headers: {
+            Authorization: "Mzk3MTU0OTMyOTU4MTY3MDUx.MTcxNjM5ODA4OQ--.c395354176",
+        } });
+        return data;
+    } catch (err) {
+        throw new Error(err.message);
+    }
 };
-search.addEventListener('click', () =>{
-  fetchData();
-})
+ search.addEventListener('click', () =>{
+  img.src=`${data()}`});
+ 
+console.log(data());
