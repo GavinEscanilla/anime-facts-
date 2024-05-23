c8302cc9022a7839ec349ad973d18284c641fe47
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-05-22 22:58:28 -0400

    created a dropdown which links to what gif you want

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-//const input = document.getElementById("search-input");
+const input = document.getElementById("input");
 const search = document.getElementById("search-button");
 const img = document.getElementById('img');
-const url = "https://waifu.it/api/v4/angry"; 
+const url = "https://waifu.it/api/v4"; 
 /*
 Replace "Your-API-Token" with the token you got from the Kohai Bot and the endpoint.
 */
 
-const fetchData = async () => {
+const fetchData = async (input) => {
     const err = "no data";
     try {
-      const res = await fetch(url, { headers: {
+      const res = await fetch(`${url}/${input}`, { headers: {
             Authorization: "Mzk3MTU0OTMyOTU4MTY3MDUx.MTcxNjM5ODA4OQ--.c395354176",
         } });
         const data = await res.json();
@@ -19,8 +19,8 @@ const fetchData = async () => {
         console.log(err);
     }
 };
- fetchData();
+ 
  console.log(11+11);
  search.addEventListener('click',() =>{
-    fetchData();
+    fetchData(input.value);
  })
